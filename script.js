37b297942f68d8a03aab39d2e316124783897258
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-02-22 14:13:17 -0500

    creates compare function

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var cardValues = [];
 var player1 = [];
 var computer = [];
+var dealDiv = document.getElementById("deal");
+
 var deck = {
   values: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
   suits: ["Clubs", "Diamonds", "Hearts", "Spades"],
   cards: [],
+  hand: [],
   build: function() {
     for (i = 0; i < this.values.length; i++){
       this.cards.push([this.values[i], "Clubs"]);
@@ -26,8 +29,20 @@ var deck = {
   deal: function(cards) {
     player1 = this.cards.splice(0, Math.floor(this.cards.length / 2));
     computer = this.cards;
+  },
+  compare: function() {
+    this.hand.push(player1.pop());
+    this.hand.push(computer.pop());
+    if (this.hand[0][0] > this.hand[1][0]){
+      console.log("Player wins");
+    }
+    else {
+      console.log("computer wins");
+    }
   }
 };
 deck.build();
 deck.shuffle();
 deck.deal();
+// deck.compare();
+dealDiv.addEventListener("click", deck.compare);
