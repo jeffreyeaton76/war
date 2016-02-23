fbaa2a8997d6a4553e18ed0623c1f4c381e1b21b
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-02-23 17:57:03 -0500

    creates separate score function not finished

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ As a developer, I should be able to create a deck, shuffle it, and deal half of 
 
 As a user, I should be able to deal two cards (one from my pile and one from the computer's) and have those  cards compared so as to determine the winner of that round.
 
-As a user, I should see my score kept somewhere on the webpage. 
+As a user, I should see my score kept somewhere on the webpage.
+
+As a user, when the cards are tied the game should deal two cards face down (one for each player) and then "flip" again and the score for that round should go up by three. 
 
 As a user, I should know when the deck has run out and who won the game.
 
--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 var player1 = [];
 var computer = [];
-var playerWins = 0;
-var computerWins = 0;
-var dealDiv = document.getElementById("deal");
+var playerWins = 26;
+var computerWins = 26;
+var wasATie = false;
+var playerWon;
+var computerWon;
+var actionDiv = document.getElementById("action");
 var playerScore = document.getElementById("player_score");
 var computerScore = document.getElementById("computer_score");
+var playerCard = document.getElementById("playerCard");
+var computerCard = document.getElementById("computerCard");
 
 var deck = {
   values: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
@@ -13,12 +18,12 @@ var deck = {
   hand: [],
   build: function() {
     for (i = 0; i < this.values.length; i++){
-      this.cards.push([this.values[i], "clubs"]);
-      this.cards.push([this.values[i], "hearts"]);
-      this.cards.push([this.values[i], "diamonds"]);
-      this.cards.push([this.values[i], "spades"]);
+      for (j = 0; j < this.suits.length; j++) {
+        this.cards.push([this.values[i], this.suits[j]]);
+      }
     }
   },
+// deck.suffle is the Fisher-Yates shuffle algorithm
   shuffle: function(cards) {
     var m = this.cards.length, t, i;
     while (m) {
@@ -29,33 +34,80 @@ var deck = {
       }
     return cards;
   },
-  deal: function() {
+  splitDeck: function() {
     player1 = this.cards.splice(0, Math.floor(this.cards.length / 2));
     computer = this.cards;
   },
-  compare: function() {
+  deal: function() {
     if (player1.length > 0) {
       deck.hand.push(player1.pop());
       deck.hand.push(computer.pop());
-      document.getElementById("playerCard").src="cards/" + deck.hand[deck.hand.length-1][0] + "_of_" + deck.hand[deck.hand.length-1][1] + ".png";
-      document.getElementById("computerCard").src="cards/" + deck.hand[deck.hand.length-2][0] + "_of_" + deck.hand[deck.hand.length-2][1] + ".png";
-      if (deck.hand[deck.hand.length-1][0] < deck.hand[deck.hand.length-2][0]){
-        playerWins++;
-        playerScore.innerHTML = playerWins;
-        console.log("Player wins! Player score is " + playerWins);
-      }
-      else {
-        computerWins++;
-        computerScore.innerHTML = computerWins;
-        console.log("Computer wins! Computer score is " + computerWins);
-      }
+      playerCard.src="cards/" + deck.hand[deck.hand.length-1][0] + "_of_" + deck.hand[deck.hand.length-1][1] + ".png";
+      computerCard.src="cards/" + deck.hand[deck.hand.length-2][0] + "_of_" + deck.hand[deck.hand.length-2][1] + ".png";
+      deck.compare();
     }
+  },
+  compare: function() {
+    if (deck.hand[deck.hand.length-1][0] == deck.hand[deck.hand.length-2][0]) {
+      deck.tie();
+      }
+    else if (deck.hand[deck.hand.length-1][0] < deck.hand[deck.hand.length-2][0]){
+      wasATie = false;
+      playerWon = true;
+      deck.score();
+      }
+    else if (deck.hand[deck.hand.length-1][0] > deck.hand[deck.hand.length-2][0]) {
+      wasATie = false;
+      computerWon = true;
+      deck.score();
+      }
     else {
-      console.log("Game over");
+      deck.gameOver();
     }
+  },
+  tie: function() {
+    actionDiv.innerHTML = "I DECLARE WAR!";
+    wasATie = true;
+    for (i = 0; i <= 3; i++) {
+      deck.hand.push(player1.pop());
+      deck.hand.push(computer.pop());
+    }
+    return wasATie;
+  },
+  score: function() {
+    if (playerWon === true && wasATie === true){
+      wasATie = false;
+      playerWon = false;
+      playerWins = playerWins - 6;
+      playerScore.innerHTML = player1.length;
+    }
+    else if (playerWon === true && wasATie === false){
+      playerWon = false;
+      playerWins = playerWins - 2;
+      playerScore.innerHTML = player1.length;
+      }
+    else if (computerWon === true && wasATie === true){
+      wasATie = false;
+      computerWon = false;
+      computerWins = computerWins - 6;
+      computerScore.innerHTML = computer.length;
+    }
+    else if (computerWon === true && wasATie === false){
+      computerWon = false;
+      computerWins = computerWins - 2;
+      computerScore.innerHTML = computer.length;
+    }
+  },
+  gameOver: function() {
+    actionDiv.innerHTML = "Game over!";
+    actionDiv.addEventListener("click", deck.gameStart);
+  },
+  gameStart: function() {
+    actionDiv.innerHTML = "DEAL!";
+    deck.build();
+    deck.shuffle();
+    deck.splitDeck();
   }
 };
-deck.build();
-deck.shuffle();
-deck.deal();
-dealDiv.addEventListener("click", deck.compare);
+deck.gameStart();
+actionDiv.addEventListener("click", deck.deal);
